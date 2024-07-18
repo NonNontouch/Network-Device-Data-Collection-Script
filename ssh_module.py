0087e723e5715a916f75cd5a5247e90aa4fe9544
NonNontouch <Nontouch16974@gmail.com>
2024-07-18 11:07:57 +0700

    function:add enable_device func
    - เพิ่ม function ในการตรวจสอบให้ device enable
    - แก้ไขโครงสร้างโปรแกรม

--- ssh_module.py
+++ ssh_module.py
@@ -8,22 +8,39 @@ class ssh_connection:
     policy = para.AutoAddPolicy()
     session = None
 
-    def __init__(self, hostname: str, username: str, password: str, port=None):
+    def __init__(self, hostname: str, username: str, password: str, port=None, enable_password=None, timeout=None, banner_timeout=None):
         self.hostname = hostname
         self.usename = username
         self.password = password
+        if enable_password != None:
+            self.enable_password = enable_password
         if port == None:
             self.port = 22
         else:
             self.port = port
 
+        if timeout == None:
+            self.timeout = 4000
+        else:
+            self.timeout = timeout
+
+        if banner_timeout == None:
+            self.banner_timeout = 1000
+        else:
+            self.banner_timeout = banner_timeout
+
+    def connect_to_device(self):
         try:
             self.connect.set_missing_host_key_policy(self.policy)
             self.connect.connect(hostname=self.hostname, port=self.port,
-                                 username=self.usename, password=self.password, banner_timeout=200)
+                                 username=self.usename, password=self.password, timeout=self.timeout, banner_timeout=self.banner_timeout)
             self.session = self.connect.invoke_shell()
-        except:
-            print("Error Connection ")
+        except OSError as e:
+            print(e)
+            return e
+        except para.SSHException as e:
+            print(e)
+            return e
 
     def find_prompt(self, output):
         last_line = output.splitlines()[-1].strip()
@@ -36,28 +53,50 @@ class ssh_connection:
         self.session.send(command + '\n')
         sleep(0.3)
         while True:
-            _output = self.get_output(self.session)
+            _output = self.get_output()
             cmd_output += _output
-            # print(_output)
             if self.find_prompt(_output):
                 break
         return cmd_output
 
-    def get_output(self, session):
-        return session.recv(65535).decode('utf-8').rstrip()
+    def enable_device(self, password: str):
+        self.session.send('enable\n')
+        sleep(0.3)
+        while True:
+            _output = self.get_output()
+            for text in ["Password:", "password:"]:
+                if text in _output:
+                    _output = self.send_command(password)
+                    break
+            if _output[-1] == "#":
+                break
+            else:
+                return False
+        return True
+
+    def get_output(self):
+        return self.session.recv(65535).decode('utf-8').rstrip()
 
     def start_get_data(self):
         console_name = self.send_command("\n")
         if console_name[-1] == ">":
-            enable_result = self.send_command("Enable")
-            for temp in ["Password", "password"]:
-                if temp in enable_result:
-                    self.send_command("REDACTED")
+            try:
+                self.enable_device(self.enable_password)
+            except TypeError:
+                return
+            except:
+                return
         connection.send_command("ter le 0")
-        run_conf = connection.send_command("show run")
+        run_conf = connection.send_command("show tech")
         print("***\n", run_conf, "\n***")
 
+    def get_tech_support(self):
+        return connection.send_command("show tech")
 
-connection = ssh_connection("10.10.40.223", "non", "REDACTED")
-connection.start_get_data()
-connection.session.close()
+
+connection = ssh_connection(hostname="10.10.40.223", username="non",
+                            password="REDACTED", enable_password="REDACTED")
+connection.connect_to_device()
+if connection.session != None:
+    connection.start_get_data()
+    connection.session.close()
