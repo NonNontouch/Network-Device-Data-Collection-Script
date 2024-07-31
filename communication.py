477f6a25f02a5299cd7998ec8a44c75a2168d107
Nontouch Mukleemart <nontouch16974@gmail.com>
2024-07-31 16:11:14 +0700

    chore: impliment facade pattern

--- communication.py
+++ communication.py
@@ -1,13 +1,77 @@
+import os
 import re
+import ssh_module as ssh
 from string import printable
 
 
 class communication:
-    def __init__(self) -> None:
-        pass
+    hostname = ""
+    username = ""
+    password = ""
+    enablepassword = ""
+    timeout = None
+    banner_timeout = None
+    port = None
+
+    connection = None
+
+    def set_hostname(self, hostname: str):
+        self.hostname = hostname
+
+    def set_port(self, port: int):
+        if port >= 65545 or port < 1:
+            return
+        self.port = port
+
+    def set_username(self, username: str):
+        self.username = username
+
+    def set_password(self, password: str):
+        self.password = password
+
+    def set_enablepassword(self, enablepassword: str):
+        self.enablepassword = enablepassword
+
+    def set_timeout(self, timeout: int):
+        if timeout <= 0:
+            return
+        self.timeout = timeout
+
+    def set_banner_timeout(self, banner_timeout: int):
+        if banner_timeout < 0:
+            return
+        self.banner_timeout = banner_timeout
+
+    def set_ssh_connection(self):
+        self.connection = ssh.ssh_connection(
+            hostname=self.hostname,
+            username=self.username,
+            password=self.password,
+            enable_password=self.enablepassword,
+            timeout=self.timeout,
+            banner_timeout=self.banner_timeout,
+        )
+
+    def send_list_command(self):
+        return
+
+    def connect_to_ssh(self):
+        self.connection.connect_to_device()
+        self.connection.send_list_command(["ter le 0", "show run"])
+
+    def get_template_name(self):
+        folder_path = "./command_template"
+        try:
+            filenames = os.listdir(folder_path)
+            print(filenames)
+
+        except FileNotFoundError:
+            print(f"Error: Folder '{folder_path}' not found.")
+            return
 
 
 class common_function:
+
     def find_prompt(self, output: str):
         if output != "":
             last_line = output.splitlines()[-1].strip()
@@ -19,17 +83,12 @@ class common_function:
         # หาคำว่า invalid หรือ command not found ถ้ามี ให้return True ถ้าไม่มี return false
         return (
             re.search(
-                r"invalid|command not found|Unrecognized", output, flags=re.IGNORECASE
+                r"invalid|command not found|Unrecognized|Unknown command|Incomplete command",
+                output,
+                flags=re.IGNORECASE,
             )
             is not None
         )
-
-    def find_prompt(self, output: str):
-        if output != "":
-            last_line = output.splitlines()[-1].strip()
-            if re.match(r"([\w-]+)(>|(?:\(config.*\))*#)", last_line):
-                return True
-        return False
 
     def remove_control_char(self, output: str):
         # ลบ control char ที่ไม่จำเป็น ให้เหลือแค่ \n\r\t
@@ -50,3 +109,8 @@ class Error:
         ):
             self.message = message
             super().__init__(self.message)
+
+
+if __name__ == "__main__":
+    test = communication()
+    test.get_template_name()
