568dbdda1118bdeb0b65aaf031b001b50539e5ea
Nontouch Mukleemart <nontouch16974@gmail.com>
2024-08-23 10:23:29 +0700

    Doc: Add doc in telnet module

--- src/telnet_module.py
+++ src/telnet_module.py
@@ -5,9 +5,16 @@ from time import sleep
 
 
 class telnet_connection:
+    """_This is class responsible for Telnet Communication which use telnetlib library._\n
+    _To use this class you must init and then use connect to device funciton._"""
     session = None
 
     def __init__(self, connection):
+        """_init the serial connection module with connection variable._
+
+        Args:
+            connection (_class connection_): _A connection class with ready to use variable._
+        """
         # pass communication object into telnet_module
         self.hostname: str = connection.hostname
         self.username: str = connection.username
@@ -18,6 +25,12 @@ class telnet_connection:
         self.banner_timeout: int = connection.banner_timeout
 
     def connect_to_device(self):
+        """_This function will try login into device via Telnet._
+
+        Raises:
+            e: _description_
+            e: _description_
+        """
         try:
             self.connect = Telnet(host=self.hostname, port=self.port, timeout=4)
 
@@ -48,6 +61,17 @@ class telnet_connection:
             self.connect.write(self.to_bytes(self.password))
 
     def send_command(self, command: str):
+        """_Send command to a host via Telnet._
+
+        Args:
+            command (_str_): _A command you want to send to this host._
+
+        Raises:
+            Error.ErrorCommand: _When command given has return with error in command result string._
+
+        Returns:
+            string: _A result from given command._
+        """
         cmd_output = ""
         self.connect.write(self.to_bytes(command))
         sleep(0.3)
@@ -61,10 +85,24 @@ class telnet_connection:
         return cmd_output
 
     def is_enable(self):
+        """_Check if device is enable._
+
+        Returns:
+            bool: _True if enabled, False if not._
+        """
         console_name = self.send_command("").splitlines()[-1].strip()
         return True if console_name[-1] == "#" else False
 
     def enable_device(self, enable_command: str, password: str):
+        """_This function will make device in Privileged EXEC mode. _
+
+        Args:
+            enable_command (str): _description_
+            password (str): _description_
+
+        Raises:
+            Error.ErrorEnable_Password: _description_
+        """
         self.connect.write(self.to_bytes(enable_command))
         sleep(0.3)
 
@@ -79,9 +117,18 @@ class telnet_connection:
             raise Error.ErrorEnable_Password(self.enable_password)
 
     def close_connection(self):
+        """_Close connection and set connect variable to None._"""
         # close connection and set it none
         self.connect = self.connect.close()
 
     @staticmethod
-    def to_bytes(line):
+    def to_bytes(line: str):
+        """return a bytes of string which also include \\n
+
+        Args:
+            line (_str_): a string that will convert to byte
+
+        Returns:
+            byte: byte string with \\n in the end
+        """
         return f"{line}\n".encode("utf-8")
