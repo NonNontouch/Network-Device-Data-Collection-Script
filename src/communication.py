1c0c5cbaece19a780e703226be4b1c8ca41e06e2
Nontouch Mukleemart <nontouch16974@gmail.com>
2024-08-27 16:01:55 +0700

    chor: change send_list_command structure

--- src/communication.py
+++ src/communication.py
@@ -172,7 +172,7 @@ class connection:
             self.serial_port_list = self.connection.list_serial_ports()
             return self.serial_port_list
 
-    def send_list_command(self, command_list_json: dict):
+    def send_list_command(self, command_dict_json: dict):
         """_Send command to device from sequnce given in dict argument._
 
         Args:
@@ -188,12 +188,17 @@ class connection:
         if self.connection == None:
             raise Error.ConnectionError
         try:
-            if "Enable Device" in command_list_json:
-                self.connection.enable_device(
-                    enable_command=command_list_json["Enable Device"],
-                    password=self.enable_password,
-                )
-                del command_list_json["Enable Device"]
+            if self.connection.is_enable() is False:
+                if "Enable Device" in command_dict_json:
+                    self.connection.enable_device(
+                        enable_command=command_dict_json["Enable Device"],
+                        password=self.enable_password,
+                    )
+                    del command_dict_json["Enable Device"]
+                else:
+                    raise Error.LoginError("Program can't enable device")
+            else:
+                pass
         except Error.ErrorCommand as e:
             # return เพราะ enable ไม่ได้
             print(e, ".While trying to enable device.", sep="")
@@ -210,15 +215,18 @@ class connection:
             # return กลับเพราะ password ผิด
             print(e)
             return
+        if "show vlt number" in command_dict_json:
+            vlt_domain = self.get_vlt_number(command_dict_json)
+            command_dict_json["show vlt status"] = f"show vlt {vlt_domain}"
+            pass
+        command_dict_json.pop("Tetminal Length 0")
+        # command_dict_json.pop("show vlt status")
+        # command_dict_json.pop("show vlt number")
+        command_dict_json.pop("show tech-support")
 
-        
-        command_list_json.pop("Tetminal Length 0")
-        command_list_json.pop("show vlt status")
-        command_list_json.pop("show vlt number")
-        command_list_json.pop("show tech-support")
+        command_list = list(command_dict_json.values())
+        command_list_json = list(command_dict_json.keys())
 
-        command_list = list(command_list_json.values())
-        command_list_json = list(command_list_json.keys())
         for i in range(len(command_list_json)):
             try:
                 command = command_list[i]
@@ -247,9 +255,8 @@ class connection:
             print(i, end=" ")
         return result
 
-    def get_vlt_number(self, command_json: dict):
-        self.connection.send_command("")
-        raw_vlt_num = self.connection.send_command(command_json["show vlt number"])
+    def get_vlt_number(self, command_dict_json: dict):
+        raw_vlt_num = self.connection.send_command(command_dict_json["show vlt number"])
         match = re.search(r"vlt-domain (\d+)", raw_vlt_num)
         if match:
             return int(match.group(1))
